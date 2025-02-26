aeaebc8d5d452f971353b5fc95cad62d2a28ceb9
Manav Chaudhari <manav@Manavs-MacBook-Pro.local>
2025-02-27 00:18:06 +0530

    Band Name Generator ✓

---
+++ Days1-10/Day1/main.py
@@ -0,0 +1,5 @@
+print("Welcome to Band Name Generator")
+city_name = input("What's name of the city you grew up in?\n")
+pet_name = input("What's your pet's name?\n").lower()
+band_name = city_name + " " + pet_name
+print(f"Your band name could be {band_name}")
