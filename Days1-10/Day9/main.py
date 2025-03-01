6345b40fa13e94e848b82e384c60549af0886513
Manav Chaudhari <vrmanav02@gmail.com>
2025-03-01 21:44:32 +0530

    Learn python dict

---
+++ Days1-10/Day9/main.py
@@ -0,0 +1,7 @@
+programming_dict = {
+    "Bug": "An error in program",
+    "Function": "A piece of code that you can easily call over and over again",
+}
+programming_dict["Loop"] = "The action of doing something over and over again"
+for key in programming_dict:
+    print(f"{key} : {programming_dict[key]}")
