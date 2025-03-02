f2e8bb7e94c292309ca903855f5eaa36b705e28f
Manav Chaudhari <vrmanav02@gmail.com>
2025-03-02 15:08:40 +0530

    Silent Auction Program ✓

--- Days1-10/Day9/main.py
+++ Days1-10/Day9/main.py
@@ -1,7 +1,28 @@
-programming_dict = {
-    "Bug": "An error in program",
-    "Function": "A piece of code that you can easily call over and over again",
-}
-programming_dict["Loop"] = "The action of doing something over and over again"
-for key in programming_dict:
-    print(f"{key} : {programming_dict[key]}")
+from os import system
+
+
+def find_highest_bidder(bidders):
+    highest_bid = 0
+    highest_bidder = ""
+    for bidder in bidders:
+        if bidders[bidder] > highest_bid:
+            highest_bidder = bidder
+            highest_bid = bidders[bidder]
+    return highest_bidder
+
+
+print("WELCOME TO SECRET AUCTION PROGRAM")
+bidders = {}
+stop_game = False
+
+while not stop_game:
+    bidder_name = input("\nWhat is your name?\n")
+    bidder_bid = int(input("What is your bid? \n$"))
+    bidders[bidder_name] = bidder_bid
+    more_players = input("Are there any more bidders? [YES/NO]\n").lower()
+    if more_players == "no":
+        stop_game = True
+    system("clear")
+
+highest_bidder = find_highest_bidder(bidders)
+print(f"Highest bidder is {highest_bidder}")
