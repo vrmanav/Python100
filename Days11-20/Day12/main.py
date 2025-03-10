4a52fa5e3bd19946a47af8ac8b0bb2b3db7d8c94
Manav Chaudhari <vrmanav02@gmail.com>
2025-03-10 23:46:07 +0530

    Number guessing ✓

--- Days11-20/Day12/main.py
+++ Days11-20/Day12/main.py
@@ -5,35 +5,42 @@ EASY_ATTEMPTS = 10
 HARD_ATTEMPTS = 5
 
 
+def set_difficulty():
+    difficulty = input("Choose difficulty [EASY/HARD]:\n")
+    if difficulty == "easy":
+        return EASY_ATTEMPTS
+    elif difficulty == "hard":
+        return HARD_ATTEMPTS
+
+
+def check_answer(secret_number, user_guess, attempts):
+    if user_guess == secret_number:
+        print("\nThat's correct 🎉 You've made the right guess!")
+    elif user_guess < secret_number:
+        print(f"Too Low.")
+        return attempts - 1
+    elif user_guess > secret_number:
+        print(f"Too High.")
+        return attempts - 1
+
+
 def number_guessing_game():
     print("* Welcome To Number Guessing Game *\n")
     print("I've thought of number between 1 and 100.")
     print("It's your task to make a correct guess of that number.\n")
-
-    # attempts_remaining = 0
-    difficulty = input("Choose difficulty [EASY/HARD]:\n")
-    if difficulty == "easy":
-        attempts_remaining = EASY_ATTEMPTS
-    elif difficulty == "hard":
-        attempts_remaining = HARD_ATTEMPTS
-    print(f"You've {attempts_remaining} attempts remaining")
-
-    continue_game = True
     secret_number = randint(1, 101)
     print(secret_number)
-    while continue_game and attempts_remaining:
+
+    attempts = set_difficulty()
+    user_guess = 0
+
+    while user_guess != secret_number:
+        print(f"You've {attempts} attempts to guess the number")
         user_guess = int(input("\nMake your guess:\n"))
-        if user_guess == secret_number:
-            print("That's correct! You've made the right guess!")
-            continue_game = False
-        elif user_guess < secret_number:
-            attempts_remaining -= 1
-            # print("Too Low")
-            print(f"Too Low. You've {attempts_remaining} attempts remaining.")
-        elif user_guess > secret_number:
-            attempts_remaining -= 1
-            # print("Too High")
-            print(f"Too High. You've {attempts_remaining} attempts remaining.")
+        attempts = check_answer(secret_number, user_guess, attempts)
+        if attempts == 0:
+            print("\n⚠️ You run out of attempts. You lose! ⚠️")
+            return
 
 
 number_guessing_game()
