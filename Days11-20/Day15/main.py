f6bc63017d6b7dc5159996a47b1e93ffacf60102
Manav Chaudhari <vrmanav02@gmail.com>
2025-03-12 10:47:27 +0530

    Implement basic functionalities

---
+++ Days11-20/Day15/main.py
@@ -0,0 +1,48 @@
+from os import system
+from time import sleep
+from menu import MENU
+
+espresso_cost = MENU["espresso"]["cost"]
+latte_cost = MENU["latte"]["cost"]
+cappuccino_cost = MENU["cappuccino"]["cost"]
+profit = 0
+
+resources = {
+    "water": 300,
+    "milk": 200,
+    "coffee": 100,
+}
+
+
+def print_report():
+    system("clear")
+    sleep(0.5)
+    print("Resources remaining\n")
+    print(f"* Water: \t{resources['water']}mL")
+    print(f"* Milk: \t{resources['milk']}mL")
+    print(f"* Coffee: \t{resources['coffee']}g")
+    print(f"* Money: \t$ {profit}")
+
+
+def greet():
+    print("Welcome to Barista Coffee")
+    print("\n\t     MENU")
+    print(f"* Espresso\t\t ${espresso_cost}")
+    print(f"* Latte\t\t\t ${latte_cost}")
+    print(f"* Cappuccino\t\t ${cappuccino_cost}")
+
+
+def coffee_maker():
+    greet()
+    order = input("\nWhat would you like to drink:\n").lower()
+    if order == "report":
+        print_report()
+    elif order == "off":
+        print("\nTurning OFF the machine ⚠️")
+        sleep(2.5)
+        print("Machine turned OFF 😴")
+    else:
+        drink = MENU[order]
+
+
+coffee_maker()
