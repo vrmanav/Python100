55a9d85dd24be1bab0143ced2e2a0391fd245d37
Manav Chaudhary <vrmanav02@gmail.com>
2025-03-14 22:54:50 +0530

    Coffee Maker Machine

--- Days11-20/Day15/main.py
+++ Days11-20/Day15/main.py
@@ -14,6 +14,15 @@ resources = {
 }
 
 
+def greet():
+    system("clear")
+    print("Welcome to Barista Coffee")
+    print("\n\t     MENU")
+    print(f"* Espresso\t\t ${espresso_cost}")
+    print(f"* Latte\t\t\t ${latte_cost}")
+    print(f"* Cappuccino\t\t ${cappuccino_cost}")
+
+
 def print_report():
     system("clear")
     sleep(0.5)
@@ -24,20 +33,38 @@ def print_report():
     print(f"* Money: \t$ {profit}")
 
 
-def greet():
-    print("Welcome to Barista Coffee")
-    print("\n\t     MENU")
-    print(f"* Espresso\t\t ${espresso_cost}")
-    print(f"* Latte\t\t\t ${latte_cost}")
-    print(f"* Cappuccino\t\t ${cappuccino_cost}")
-
-
 def is_resource_sufficient(drink_ingredients):
     for item in drink_ingredients:
         if drink_ingredients[item] >= resources[item]:
             print(f"\n⚠️ Sorry there is not sufficient {item}")
             return False
     return True
+
+
+def process_coins():
+    print("\nPlease insert coins 🔽")
+    total = int(input("How many quarters: ")) * 0.25
+    total += int(input("How many dimes: ")) * 0.1
+    total += int(input("How many nickles: ")) * 0.05
+    total += int(input("How many pennies: ")) * 0.01
+    return total
+
+
+def is_transaction_successful(payment, drink_cost):
+    if payment >= drink_cost:
+        change = round(payment - drink_cost, 2)
+        print(f"\nHere's your ${change} change & ", end="")
+        return True
+    else:
+        print("SORRY! that's not enough money. Money refunded.")
+        return False
+
+
+def make_coffee(drink):
+    drink_ingredients = MENU[drink]["ingredients"]
+    for item in drink_ingredients:
+        resources[item] -= drink_ingredients[item]
+    print(f"your {drink}. ENJOY ☕️")
 
 
 def coffee_maker():
@@ -52,7 +79,11 @@ def coffee_maker():
     else:
         drink = MENU[order]
         if is_resource_sufficient(drink["ingredients"]):
-            
+            payment = process_coins()
+            if is_transaction_successful(payment, drink["cost"]):
+                global profit
+                profit += drink["cost"]
+                make_coffee(order)
 
 
 coffee_maker()
