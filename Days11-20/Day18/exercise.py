2a107fe2bb90326ff49d00cca71c321c678e58be
Manav Chaudhary <vrmanav02@gmail.com>
2025-03-22 23:18:34 +0530

    Add 2 more exercises

--- Days11-20/Day18/exercise.py
+++ Days11-20/Day18/exercise.py
@@ -1,6 +1,17 @@
 from turtle import Turtle, Screen
+import random
 
 tim = Turtle("turtle")
+colors = [
+    "cyan",
+    "deep sky blue",
+    "orange red",
+    "yellow",
+    "dark violet",
+    "deep pink",
+    "lavender",
+    "spring green",
+]
 
 # * Exercise 1
 # for step in range(4):
@@ -19,6 +30,28 @@ tim = Turtle("turtle")
 #     tim.penup()
 #     tim.forward(5)
 #     tim.pendown()
+
+
+# * Exercise 4 - Shape maker
+# def draw_shape(no_of_sides):
+#     for step in range(no_of_sides):
+#         tim.forward(100)
+#         tim.right(360 / no_of_sides)
+# for sides in range(3, 8):
+#     tim.color(random.choice(colors))
+#     draw_shape(sides)
+
+
+# * Exercise 5 - Random walk generator
+# directions = [0, 90, 180, 270]
+# tim.hideturtle()
+# tim.speed(10)
+# tim.pensize(15)
+# for step in range(200):
+#     tim.color(random.choice(colors))
+#     tim.forward(30)
+#     tim.setheading(random.choice(directions))
+
 
 screen = Screen()
 screen.exitonclick()
