81c40eabf3e4a480ea0ca2ac34df9030f95511dc
Manav Chaudhary <vrmanav02@gmail.com>
2025-03-22 12:34:41 +0530

    Complete 3 exercises

---
+++ Days11-20/Day18/exercise.py
@@ -0,0 +1,24 @@
+from turtle import Turtle, Screen
+
+tim = Turtle("turtle")
+
+# * Exercise 1
+# for step in range(4):
+#     tim.forward(100)
+#     tim.left(90)
+
+# * Exercise 2
+# import heroes
+# import villains
+
+# print(f"'{heroes.gen()}' V/S '{villains.gen()}'")
+
+# * Exercise 3
+# for step in range(20):
+#     tim.forward(5)
+#     tim.penup()
+#     tim.forward(5)
+#     tim.pendown()
+
+screen = Screen()
+screen.exitonclick()
